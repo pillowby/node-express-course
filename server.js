ec1909761088e943287f06dbf46621131ffed9e9
pillowby <boyi93@hotmail.com>
2020-08-04 03:28:38 +0800

    add login POST route

--- server.js
+++ server.js
@@ -22,6 +22,30 @@ app.get('/users/:id',function(req,res){
 	})
 })
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
+})
+
 
 app.listen(8000,function(){
     console.log("server is listening")
